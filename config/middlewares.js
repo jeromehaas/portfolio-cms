ad24c75896ce478855a688ce897dd55766707873
jeromehaas <hello@jeromehaas.ch>
2024-06-21 22:57:29 +0200

    Setup: upload config

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,25 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = ({ env }) => [
+	'strapi::errors',
+	{
+		name: 'strapi::security',
+		config: {
+			contentSecurityPolicy: {
+				useDefaults: true,
+				directives: {
+					'connect-src': ['\'self\'', 'https:'],
+					'img-src': ['\'self\'', 'data:', 'blob:', 'strapi.io', 'market-assets.strapi.io', `${ env('SPACE_URL')}`],
+					'media-src': ['\'self\'', 'data:', 'blob:', 'strapi.io', 'market-assets.strapi.io', `${ env('SPACE_URL')}`],
+					upgradeInsecureRequests: null,
+				},
+			},
+		},
+	},
+	'strapi::cors',
+	'strapi::poweredBy',
+	'strapi::logger',
+	'strapi::query',
+	'strapi::body',
+	'strapi::session',
+	'strapi::favicon',
+	'strapi::public',
 ];
--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,20 @@
-module.exports = () => ({});
+module.exports = ({env}) => ({
+	upload: {
+		config: {
+			provider: 'aws-s3',
+			providerOptions: {
+				s3Options: {
+					credentials: {
+						accessKeyId: env('SPACE_ACCESS_KEY_ID'),
+						secretAccessKey: env('SPACE_ACCESS_SECRET'),
+					},
+					endpoint: env('SPACE_ENDPOINT'),
+					region: 'eu-central-1',
+					params: {
+						Bucket: env('SPACE_BUCKET'),
+					},
+				},
+			},
+		},
+	},
+});
